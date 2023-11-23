b5c5719f36711b5c66b8945476d387839960691f
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-11-23 13:21:52 +0700

    create models

---
+++ personal quiz/Model/Answer.swift
@@ -0,0 +1,13 @@
+//
+//  Answer.swift
+//  personal quiz
+//
+//  Created by D i on 23.11.2023.
+//
+
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+    
+}
---
+++ personal quiz/Model/ResponseType.swift
@@ -0,0 +1,14 @@
+//
+//  ResponseType.swift
+//  personal quiz
+//
+//  Created by D i on 23.11.2023.
+//
+
+
+enum ResponceType {
+    case single
+    case multiple
+    case ranged
+    
+}
